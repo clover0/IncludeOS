632a4bf5eafe85b7c392bb7762e4f58a98edbff5
koenuma <-->
2021-03-08 20:43:12 +0900

    clean up

--- app1/install_to_bv.sh
+++ app1/install_to_bv.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-rm -r ~/programs/my_bitvisor/process/includeos.bin
-rm -r ~/programs/my_bitvisor/process/includeos.bin.s
+BASEDIR="~/programs"
 
-cp hello ~/programs/my_bitvisor/process/includeos
+cp hello $BASEDIR/my_bitvisor/process/includeos
 
 echo "done instal includeos with app binary to bitvisor."
